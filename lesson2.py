4a8c8fd94ca4548cbe80f49951212892ae69d249
Cengizhan Yanıkcı <95139899+CengizhanYanikci@users.noreply.github.com>
2025-08-24 00:12:05 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,13 @@
+print('Python is\'n snake')
+#↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+#Disables the ' symbol after the \ symbol
+#----------------------------------------
+
+
+print('Python"is not snake')
+
+
+
+
+if 19>5:
+    print("16 higher than 5")
