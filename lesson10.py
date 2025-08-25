b900051491827d3b40630bbd1d84070eeb21bcff
Cengizhan Yanıkcı <95139899+CengizhanYanikci@users.noreply.github.com>
2025-08-25 23:27:41 +0300

    Add files via upload

---
+++ lesson10.py
@@ -0,0 +1,33 @@
+import math
+
+
+x=math.pi
+print("-------------------Calculator (Circle Area)-------------------")
+
+radiusCircle=float(input("Radius : "))    #radius
+
+
+print("Area",x*(radiusCircle**2))
+
+
+          
+#         ***********     
+#      *****       *****  
+#     ***             *** 
+#    ***               ***
+#    **           5cm   **
+#    **         --------**     ====> Area is "78.53981633974483"
+#    **                 **
+#    ***               ***
+#     ***             *** 
+#      *****       *****  
+#         ***********     
+
+
+print("-------------------Calculator (Plus)-------------------")
+
+
+num1=float(input("Number 1 : "))
+num2=float(input("Number 2 :"))
+sum=num1 + num2
+print("Result: ", sum)
---
+++ lesson9.py
@@ -0,0 +1,29 @@
+x=str("Hello Python")  #string func
+print(x)
+print(type(x))
+#---------------------------------------------------------
+
+x=int("187")  # type conversion   string to integer
+print(x)
+print(type(x))
+#---------------------------------------------------------
+
+x=int(False)
+print(x)
+print(type(x))   #False = 0   (Binary System)
+#---------------------------------------------------------
+
+x=int(True)
+print(x)
+print(type(x))   #True=1  (Binary System)
+#---------------------------------------------------------
+x=complex(2,3)   # 2 => reel num      3=>imaginare number    | ( ,x) =>0+xj  |  /  | (x, ) =>  x+0j  |   /   |    (x,y) => x + yj
+print(x)
+print(type(x)) 
+#---------------------------------------------------------
+x=bool(-81293612739238123981623) 
+print(x)
+print(type(x)) 
+# The number 0 corresponds to the value False, while all 
+# numbers written except the number 0 (including negative and positive) correspond to the value True.
+#---------------------------------------------------------
