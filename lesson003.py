dd6e544690a112bd70a97352057dbc0b62a35389
Cengizhan Yanıkcı <95139899+CengizhanYanikci@users.noreply.github.com>
2025-08-25 23:36:52 +0300

    Add files via upload

---
+++ lesson003.py
@@ -0,0 +1,61 @@
+
+# In other languages, variable assignment is done as follows:
+# String mama= "Iam hungary"
+# İnt papa = 666
+
+#-------------------------------------------------------------------------------------
+
+
+
+#But in Python, variable assignment is done like this:
+x=1
+y=2
+z="Hello"
+print(x,y,z)
+
+
+
+
+#-------------------------------------------------------------------------------------
+
+
+
+#Difference between string variable number and integer variable number
+
+q=int(1)
+w=2
+print(q+w)
+# Look terminal :D 7 
+
+
+
+#-------------------------------------------------------------------------------------
+
+'''
+q=str(1)
+2=int(2)
+
+print(q+w)
+'''
+#↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+#Delete all single close quotation mark
+#We have a syntax error because the variable q is a string value and the variable w is an integer value. We cannot add an integer and a string value.
+
+#-------------------------------------------------------------------------------------
+
+#How to find out the type of a variable
+
+print(type(x))
+print(type(y))
+#-------------------------------------------------------------------------------------
+
+#case sensitive
+
+f=1
+F=2
+
+print(f)
+print(F)
+
+#x and X is not same variable
+#-------------------------------------------------------------------------------------
